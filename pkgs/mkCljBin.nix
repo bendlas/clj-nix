bd0bc6f5d06b4b543a9a8019d3cc5ecac4af6ad7
Herwig Hochleitner <herwig@bendlas.net>
2023-07-17 13:01:43 +0200

    passthru deps-cache

--- pkgs/mkCljBin.nix
+++ pkgs/mkCljBin.nix
@@ -104,7 +104,7 @@ stdenv.mkDerivation ({
   javaOpts = lib.escapeShellArgs java-opts;
 
   passthru = {
-    inherit main-ns fullId groupId artifactId javaMain;
+    inherit deps-cache main-ns fullId groupId artifactId javaMain;
   };
 
   preBuildPhases = [ "preBuildPhase" ];
